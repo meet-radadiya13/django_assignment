0fd5b4705f1ec27b9f4c745eb28edef4a6068ed3
meet-radadiya13 <meet.r@simformsolutions.com>
2023-06-05 12:15:44 +0530

    Improved pagination using elided pages

--- project/views.py
+++ project/views.py
@@ -2,7 +2,6 @@ import datetime
 
 from django.contrib import messages
 from django.contrib.auth.decorators import login_required
-from django.core import serializers
 from django.core.paginator import Paginator
 from django.db.models import Q
 from django.http import JsonResponse
@@ -10,17 +9,16 @@ from django.shortcuts import render, redirect
 from django.views.decorators.http import require_POST
 
 from authentication.models import User
-from project.encoder import LazyEncoder
 from project.models import Project
 
 
 # Create your views here.
 @login_required
-def view_projects(request, page_no=1):
+def view_projects(request, page_no):
     current_user = request.user
     my_projects = Project.objects.filter(Q(created_by=current_user) | Q(assign=current_user))
     context = {}
-    paginator = Paginator(my_projects, 6)
+    paginator = Paginator(my_projects, 2)
     page_obj = paginator.get_page(page_no)
     context["page_obj"] = page_obj.object_list
     context["has_next"] = page_obj.has_next()
@@ -34,6 +32,7 @@ def view_projects(request, page_no=1):
     else:
         context["previous_page_no"] = 1
     context["last_page"] = page_obj.paginator.num_pages
+    context["elided_pages"] = paginator.get_elided_page_range(page_no, on_each_side=1, on_ends=2)
     context["current_page"] = page_obj.number
     return render(request, 'project/project.html', context)
 
@@ -62,8 +61,8 @@ def insert_projects(request):
     project.name = project_name
     project.acronym = project_acronym
     year, month, date = dead_line.split('-')
-    d = datetime.date(int(year), int(month), int(date))
-    project.dead_line = d
+    _dead_line = datetime.date(int(year), int(month), int(date))
+    project.dead_line = _dead_line
     project.tags = tags
     project.description = project_description
     if completed:
@@ -107,8 +106,8 @@ def update_projects(request):
     for user_obj in users:
         project.assign.add(user_obj[0])
     year, month, date = dead_line.split('-')
-    d = datetime.date(int(year), int(month), int(date))
-    project.dead_line = d
+    _dead_line = datetime.date(int(year), int(month), int(date))
+    project.dead_line = _dead_line
     project.description = description
     project.updated_by = request.user
     if completed:
@@ -119,10 +118,43 @@ def update_projects(request):
     return redirect("view_projects", page_no=1)
 
 
+# noinspection DuplicatedCode
 @login_required
 def search_projects(request):
     query = request.GET.get('query')
+    page_no = request.GET.get('page_no')
+    print(page_no)
+    if page_no == 'undefined':
+        page_no = 1
+    context = {}
     current_user = request.user
-    projects = Project.objects.filter(Q(name__icontains=query) & (Q(created_by=current_user) | Q(assign=current_user)))
-    data = serializers.serialize('json', projects, cls=LazyEncoder)
-    return JsonResponse(data, safe=False)
+    projects = Project.objects. \
+        filter(Q(created_by=current_user) | Q(assign=current_user)).values('pk', 'name', 'acronym',
+                                                                           'assign__username',
+                                                                           'is_completed',
+                                                                           'dead_line',
+                                                                           'description',
+                                                                           'tags',
+                                                                           'created_by__username',
+                                                                           'updated_by__username',
+                                                                           'created_at',
+                                                                           'updated_at', )
+    if query is not None:
+        projects = projects.filter(Q(name__icontains=query))
+    paginator = Paginator(projects, 6)
+    page_obj = paginator.get_page(page_no)
+    context["page_obj"] = list(page_obj.object_list)
+    context["has_next"] = page_obj.has_next()
+    if context["has_next"]:
+        context["next_page_no"] = page_obj.next_page_number()
+    else:
+        context["next_page_no"] = 1
+    context["has_previous"] = page_obj.has_previous()
+    if context["has_previous"]:
+        context["previous_page_no"] = page_obj.previous_page_number()
+    else:
+        context["previous_page_no"] = 1
+    context["last_page"] = page_obj.paginator.num_pages
+    context["current_page"] = page_obj.number
+    print(context)
+    return JsonResponse(context, safe=False)
