532df472f94106bf6bb6746b4a26394618613bc4
Dev Kapadia <dev.k@simformsolutions.com>
2023-06-06 16:59:30 +0530

    Added Task filter and searching functionality with pagination.

--- project/urls.py
+++ project/urls.py
@@ -1,14 +1,18 @@
-from django.contrib.auth.decorators import login_required
 from django.urls import path
-from django.views.generic import TemplateView
 
 from project import views
 
 urlpatterns = [
-    path("view_projects/<page_no>/", views.view_projects, name="view_projects"),
+    path(
+        "view_projects/<page_no>/", views.view_projects, name="view_projects"
+    ),
     path("add_projects/", views.add_projects, name="add_projects"),
     path("insert_projects/", views.insert_projects, name="insert_projects"),
-    path("edit_projects/<int:project_id>/", views.edit_projects, name="edit_projects"),
+    path(
+        "edit_projects/<int:project_id>/",
+        views.edit_projects,
+        name="edit_projects",
+    ),
     path("update_projects/", views.update_projects, name="update_projects"),
     path("search_projects/", views.search_projects, name="search_projects"),
 
@@ -17,6 +21,7 @@ urlpatterns = [
     path("insert_tasks/", views.insert_tasks, name="insert_tasks"),
     path("edit_tasks/<int:task_id>/", views.edit_tasks, name="edit_tasks"),
     path("update_tasks/", views.update_tasks, name="update_tasks"),
-    # path("task_filter/", views.task_filter, name = "task_filter")
+    path("search_tasks/", views.search_tasks, name="search_tasks"),
+    path("filter_tasks/", views.filter_tasks, name="filter_tasks"),
 
 ]
--- project/views.py
+++ project/views.py
@@ -2,7 +2,6 @@ import datetime
 
 from django.contrib import messages
 from django.contrib.auth.decorators import login_required
-from django.core import serializers
 from django.core.paginator import Paginator
 from django.db.models import Q
 from django.http import JsonResponse
@@ -10,18 +9,20 @@ from django.shortcuts import render, redirect
 from django.views.decorators.http import require_POST
 
 from authentication.models import User
-from project.encoder import LazyEncoder
 from project.models import Project, Task
 
 
 # Create your views here.
 @login_required
-def view_projects(request, page_no=1):
+def view_projects(request, page_no):
     current_user = request.user
-    my_projects = Project.objects.filter(Q(created_by=current_user) | Q(assign=current_user))
+    my_projects = Project.objects.filter(
+        Q(created_by=current_user) | Q(assign=current_user)
+    )
     context = {}
     paginator = Paginator(my_projects, 6)
     page_obj = paginator.get_page(page_no)
+    context["ELLIPSIS"] = page_obj.paginator.ELLIPSIS
     context["page_obj"] = page_obj.object_list
     context["has_next"] = page_obj.has_next()
     if context["has_next"]:
@@ -34,36 +35,41 @@ def view_projects(request, page_no=1):
     else:
         context["previous_page_no"] = 1
     context["last_page"] = page_obj.paginator.num_pages
+    context["elided_pages"] = paginator.get_elided_page_range(
+        page_no, on_each_side=1, on_ends=2
+    )
     context["current_page"] = page_obj.number
-    return render(request, 'project/project.html', context)
+    return render(request, "project/project.html", context)
 
 
 @login_required
 def add_projects(request):
-    users = User.objects.exclude(username=request.user.username)
-    context = {'users': users}
-    return render(request, 'project/add_project.html', context)
+    users = User.objects.exclude(
+        Q(username=request.user.username) | Q(is_superuser=True)
+    )
+    context = {"users": users}
+    return render(request, "project/add_project.html", context)
 
 
 @login_required
 @require_POST
 def insert_projects(request):
-    project_name = request.POST.get('project_name')
-    project_acronym = request.POST.get('project_acronym')
-    project_assignee = request.POST.getlist('assignee')
-    dead_line = request.POST.get('dead_line')
-    tags = request.POST.getlist('tags')
-    project_description = request.POST.get('project_desc')
-    completed = request.POST.get('completed')
+    project_name = request.POST.get("project_name")
+    project_acronym = request.POST.get("project_acronym")
+    project_assignee = request.POST.getlist("assignee")
+    dead_line = request.POST.get("dead_line")
+    tags = request.POST.getlist("tags")
+    project_description = request.POST.get("project_desc")
+    completed = request.POST.get("completed")
     project = Project()
     users = []
     for user in project_assignee:
         users.append(User.objects.filter(username=user))
     project.name = project_name
     project.acronym = project_acronym
-    year, month, date = dead_line.split('-')
-    d = datetime.date(int(year), int(month), int(date))
-    project.dead_line = d
+    year, month, date = dead_line.split("-")
+    _dead_line = datetime.date(int(year), int(month), int(date))
+    project.dead_line = _dead_line
     project.tags = tags
     project.description = project_description
     if completed:
@@ -81,24 +87,32 @@ def insert_projects(request):
 
 @login_required
 def edit_projects(request, project_id):
-    if Project.objects.filter(Q(id=project_id) & Q(created_by=request.user)).exists():
+    if Project.objects.filter(
+            Q(id=project_id) &
+            Q(created_by=request.user)).exists():
         projects = Project.objects.get(id=project_id)
-        users = User.objects.exclude(Q(username=request.user.username) | Q(is_active=False))
-        context = {'projects': projects, 'users': users}
-        return render(request, 'project/edit_projects.html', context)
+        users = User.objects.exclude(
+            Q(username=request.user.username)
+            | Q(is_active=False)
+            | Q(is_superuser=True)
+        )
+        context = {"projects": projects, "users": users}
+        return render(request, "project/edit_projects.html", context)
     else:
-        messages.error(request, "You do not have permission to edit this project.")
+        messages.error(
+            request, "You do not have permission to edit this project."
+        )
         return redirect("view_projects", page_no=1)
 
 
 @login_required
 @require_POST
 def update_projects(request):
-    project_id = request.POST.get('project_id')
-    assignee = request.POST.getlist('assignee')
-    dead_line = request.POST.get('dead_line')
-    description = request.POST.get('project_desc')
-    completed = request.POST.get('completed')
+    project_id = request.POST.get("project_id")
+    assignee = request.POST.getlist("assignee")
+    dead_line = request.POST.get("dead_line")
+    description = request.POST.get("project_desc")
+    completed = request.POST.get("completed")
     project = Project.objects.get(id=project_id)
     project.assign.clear()
     users = []
@@ -106,9 +120,9 @@ def update_projects(request):
         users.append(User.objects.filter(username=user))
     for user_obj in users:
         project.assign.add(user_obj[0])
-    year, month, date = dead_line.split('-')
-    d = datetime.date(int(year), int(month), int(date))
-    project.dead_line = d
+    year, month, date = dead_line.split("-")
+    _dead_line = datetime.date(int(year), int(month), int(date))
+    project.dead_line = _dead_line
     project.description = description
     project.updated_by = request.user
     if completed:
@@ -121,11 +135,46 @@ def update_projects(request):
 
 @login_required
 def search_projects(request):
-    query = request.GET.get('query')
+    query = request.GET.get("query")
+    page_no = request.GET.get("page_no")
+    if page_no == "undefined":
+        page_no = 1
+    context = {}
     current_user = request.user
-    projects = Project.objects.filter(Q(name__icontains=query) & (Q(created_by=current_user) | Q(assign=current_user)))
-    data = serializers.serialize('json', projects, cls=LazyEncoder)
-    return JsonResponse(data, safe=False)
+    projects = Project.objects.filter(
+        Q(created_by=current_user) | Q(assign=current_user)
+    ).values(
+        "pk",
+        "name",
+        "acronym",
+        "assign__username",
+        "is_completed",
+        "dead_line",
+        "description",
+        "tags",
+        "created_by__username",
+        "updated_by__username",
+        "created_at",
+        "updated_at",
+    )
+    if query is not None:
+        projects = projects.filter(Q(name__icontains=query))
+    paginator = Paginator(projects, 6)
+    page_obj = paginator.get_page(page_no)
+    context["page_obj"] = list(page_obj.object_list)
+    context["has_next"] = page_obj.has_next()
+    if context["has_next"]:
+        context["next_page_no"] = page_obj.next_page_number()
+    else:
+        context["next_page_no"] = 1
+    context["has_previous"] = page_obj.has_previous()
+    if context["has_previous"]:
+        context["previous_page_no"] = page_obj.previous_page_number()
+    else:
+        context["previous_page_no"] = 1
+    context["last_page"] = page_obj.paginator.num_pages
+    context["current_page"] = page_obj.number
+    return JsonResponse(context, safe=False)
 
 #For Tasks.
 @login_required()
@@ -137,21 +186,10 @@ def view_tasks(request, page_no=1):
 
     current_user = request.user
     my_tasks = Task.objects.filter(Q(assign=current_user))
-    projects= Project.objects.all()
+    projects = Project.objects.all()
 
     context = {}
     context["projects"] = projects
-
-    if request.GET.get('task_filter'):
-        featured_filter = request.GET.get('task_filter')
-        listings = Task.objects.filter(project=featured_filter)
-    else:
-        listings = Task.objects.all()
-
-    context["listings"] = listings
-
-
-
 
     paginator = Paginator(my_tasks, 10)
     page_obj = paginator.get_page(page_no)
@@ -169,6 +207,7 @@ def view_tasks(request, page_no=1):
     context["last_page"] = page_obj.paginator.num_pages
     context["current_page"] = page_obj.number
     return render(request, 'task/task.html', context)
+
 
 
 @login_required
@@ -206,20 +245,94 @@ def insert_tasks(request):
 
     return redirect("view_tasks")
 
+
 @login_required
 def edit_tasks(request, task_id):
-        pass
+    pass
 
 
 @login_required
 @require_POST
 def update_tasks(request):
-        pass
+    pass
+
+@login_required
+def search_tasks(request):
+    query = request.GET.get('query')
+    page_no = request.GET.get('page_no')
+    print(page_no)
+    if page_no == 'undefined':
+        page_no = 1
+    context = {}
+    current_user = request.user
+    tasks = Task.objects. \
+        filter(Q(assign=current_user)).values('pk', 'name', 'task_acronym',
+                                              'task_type',
+                                              'project__name',
+                                              'task_priority',
+                                              'task_status',
+                                              )
+    if query is not None:
+        tasks = tasks.filter(Q(name__icontains=query))
+
+    paginator = Paginator(tasks, 10)
+    page_obj = paginator.get_page(page_no)
+    context["page_obj"] = list(page_obj.object_list)
+    context["has_next"] = page_obj.has_next()
+    if context["has_next"]:
+        context["next_page_no"] = page_obj.next_page_number()
+    else:
+        context["next_page_no"] = 1
+    context["has_previous"] = page_obj.has_previous()
+    if context["has_previous"]:
+        context["previous_page_no"] = page_obj.previous_page_number()
+    else:
+        context["previous_page_no"] = 1
+    context["last_page"] = page_obj.paginator.num_pages
+    context["current_page"] = page_obj.number
+    print(context)
+    print(tasks)
+    return JsonResponse(context, safe=False)
+
+@login_required
+def filter_tasks(request):
+    query = request.GET.get('query')
+    page_no = request.GET.get('page_no')
+    print(page_no)
+    if page_no == 'undefined':
+        page_no = 1
+    context = {}
+    current_user = request.user
+    tasks = Task.objects. \
+        filter(Q(assign=current_user)).values('pk', 'name', 'task_acronym',
+                                              'task_type',
+                                              'project__name',
+                                              'task_priority',
+                                              'task_status',
+                                              )
+    if query is not None:
+        tasks = tasks.filter(Q(project__name=query))
+
+    paginator = Paginator(tasks, 10)
+    page_obj = paginator.get_page(page_no)
+    context["page_obj"] = list(page_obj.object_list)
+    context["has_next"] = page_obj.has_next()
+    if context["has_next"]:
+        context["next_page_no"] = page_obj.next_page_number()
+    else:
+        context["next_page_no"] = 1
+    context["has_previous"] = page_obj.has_previous()
+    if context["has_previous"]:
+        context["previous_page_no"] = page_obj.previous_page_number()
+    else:
+        context["previous_page_no"] = 1
+    context["last_page"] = page_obj.paginator.num_pages
+    context["current_page"] = page_obj.number
+    print(context)
+    print(tasks)
+    return JsonResponse(context, safe=False)
 
 
-# @login_required
-# def task_filter(request):
-#     projects = Project.objects.all()
-#     context = {'projects': projects}
-#     print(context)
-#     return render(request, 'task/task.html', context)
+
+
+
