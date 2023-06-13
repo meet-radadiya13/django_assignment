914371991df4d6d429633b5b1c0da06199a75240
meet-radadiya13 <meet.r@simformsolutions.com>
2023-06-13 15:54:33 +0530

    Removed Email Credentials

--- django_assignment/settings.py
+++ django_assignment/settings.py
@@ -138,8 +138,7 @@ EMAIL_BACKEND = 'django.core.mail.backends.smtp.EmailBackend'
 EMAIL_HOST = 'smtp.gmail.com'
 EMAIL_PORT = 587
 EMAIL_USE_TLS = True
-EMAIL_HOST_USER = 'fantasticsingh99@gmail.com'
-EMAIL_HOST_PASSWORD = 'zmadxyepxibthlpb'
+
 
 # Default primary key field type
 # https://docs.djangoproject.com/en/4.2/ref/settings/#default-auto-field
